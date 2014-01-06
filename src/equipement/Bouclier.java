02d655f8b38494f842a99f0611dc43f3e6f0fd3d
PierreBeauhaire <beauhaire.pierre@gmail.com>
2014-01-06 15:33:58 +0100

    Le bouclier augmente la défense

---
+++ src/equipement/Bouclier.java
@@ -0,0 +1,12 @@
+package equipement;
+
+public class Bouclier extends Equipement {
+	
+	/**
+	 * Constructeur bouclier
+	 */
+	public Bouclier() {
+		super("bouclier", new Caracteristiques(0, 0, 10, 0, 0));
+	}
+	
+}
