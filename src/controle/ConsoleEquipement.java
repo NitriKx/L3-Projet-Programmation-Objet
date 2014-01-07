3742619f5c226ec886fcb467805094daf57f5cfe
Florentin Le Moal <florentin.lemoal@gmail.com>
2014-01-07 16:11:03 +0100

    Ne plus quitter le thread après la récupération d’un objet et diminution de la vie d’un objet

--- src/controle/ConsoleEquipement.java
+++ src/controle/ConsoleEquipement.java
@@ -97,7 +97,7 @@ public class ConsoleEquipement extends UnicastRemoteObject implements IConsole {
 	 */
 	public void shutDown(String cause) throws RemoteException {
 		System.out.println("Console "+refRMI+" deconnectee : "+cause);
-		System.exit(1);
+//		System.exit(1);
 	}
 
 	public Element getElement() throws RemoteException {
@@ -114,6 +114,7 @@ public class ConsoleEquipement extends UnicastRemoteObject implements IConsole {
 	
 
 	public void perdreVie(int viePerdue) throws RemoteException {
+		this.elem.setVie(this.elem.getVie()-viePerdue);
 	}
 	
 	public void ramasserObjet(IConsole objet) throws RemoteException {
