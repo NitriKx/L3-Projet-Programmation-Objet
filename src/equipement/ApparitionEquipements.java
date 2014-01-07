09e1f0b4b6964b6dad7ff06ec6701b1cc8cbb8df
PierreBeauhaire <beauhaire.pierre@gmail.com>
2014-01-07 10:10:30 +0100

    Faire apparaitre un équipement

---
+++ src/equipement/ApparitionEquipements.java
@@ -0,0 +1,80 @@
+package equipement;
+
+import individu.Element;
+import interfaceGraphique.VueElement;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Random;
+
+import controle.ConsoleEquipement;
+import serveur.Arene;
+
+public class ApparitionEquipements extends Thread {
+	
+	// private UtilitaireConsole console = new UtilitaireConsole();
+	private int intervalleApparitions;
+	
+	enum ListeEquipements {
+		BOTTES,
+		BOUCLIER,
+		CASQUE,
+		EPEE,
+		MASSE,
+		PIECES,
+		POTION
+	}
+	
+	public ApparitionEquipements(int intervalleApparitions) {
+		this.intervalleApparitions = intervalleApparitions;
+	}
+	
+	@Override
+	public void run() {
+		
+		try {
+		
+			int randomX = (int)(Math.random() * Arene.tailleAreneX);	// on tire l'abscisse d'une position au hasard
+			int randomY = (int)(Math.random() * Arene.tailleAreneY);	// on tire l'ordonnee d'une position au hasard
+			
+			ListeEquipements equipementPossibles[] = ListeEquipements.values();
+			ListeEquipements equipementAFaireApparaitre = equipementPossibles[new Random().nextInt(equipementPossibles.length)];
+			Element element = null;
+			switch(equipementAFaireApparaitre) {
+			case BOTTES :
+				element = new Bottes();
+				break;
+			case BOUCLIER :
+				element = new Bottes();
+				break;
+			case CASQUE :
+				element = new Bottes();
+				break;
+			case EPEE:
+				element = new Epee();
+				break;
+			case MASSE :
+				element = new Bottes();
+				break;
+			case PIECES :
+				element = new Bottes();
+				break;
+			case POTION:
+				break;
+			default:
+				break;
+			}
+			
+			ConsoleEquipement ce = new ConsoleEquipement(element, randomX, randomY);
+			
+			Thread.sleep(this.intervalleApparitions);
+			
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+			try { Thread.sleep(this.intervalleApparitions); } catch (InterruptedException e1) {}
+		}
+	}
+
+}
