a32bff1613ca396dc1b1bb431277b113cdc3dc7d
Florentin Le Moal <florentin.lemoal@gmail.com>
2014-01-07 10:33:29 +0100

    Ajout des stratégie de recherche de Personnages ou d’Équipements

--- src/controle/Strategie.java
+++ src/controle/Strategie.java
@@ -1,11 +1,13 @@
 package controle;
 
+import individu.Personnage;
 import interfaceGraphique.VueElement;
 
 import java.awt.Point;
 import java.util.HashMap;
 import java.util.Hashtable;
 
+import equipement.Equipement;
 import utilitaires.UtilitaireConsole;
 
 public class Strategie {
@@ -35,5 +37,35 @@ public class Strategie {
 		resultat.put((Integer)distPlusProche, cible);
 		
 		return resultat;
+	}
+
+	public static HashMap<Integer, HashMap<Integer,VueElement>> chercherEquipementProche(VueElement ve, Hashtable<Integer,VueElement> voisins){
+		Hashtable<Integer,VueElement> voisinsEquip = new Hashtable<Integer, VueElement>();
+		// Recopier voisins
+		// Enlever les personnages des voisins
+		for (Integer ref : voisins.keySet()) {
+			VueElement veTemp = voisins.get(ref);
+			if (veTemp.getElement() instanceof Equipement) {
+				voisinsEquip.put(ref, veTemp);
+			}
+		}
+		
+		// Chercher elements le plus proche avec voisins
+		return chercherElementProche(ve, voisins);
+	}
+
+	public static HashMap<Integer, HashMap<Integer,VueElement>> chercherPersonnageProche(VueElement ve, Hashtable<Integer,VueElement> voisins){
+		Hashtable<Integer,VueElement> vonsinsPerso = new Hashtable<Integer, VueElement>();
+		// Recopier voisins
+		// Enlever les équipements des voisins
+		for (Integer ref : voisins.keySet()) {
+			VueElement veTemp = voisins.get(ref);
+			if (veTemp.getElement() instanceof Personnage) {
+				vonsinsPerso.put(ref, veTemp);
+			}
+		}
+		
+		// Chercher elements le plus proche avec voisins
+		return chercherElementProche(ve, voisins);
 	}
 }
