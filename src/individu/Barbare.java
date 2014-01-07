bf53a93a97d1093db32060ee166ff37f0f45e92e
PierreBeauhaire <beauhaire.pierre@gmail.com>
2014-01-07 16:22:27 +0100

    Commentaires Barbare

--- src/individu/Barbare.java
+++ src/individu/Barbare.java
@@ -9,11 +9,16 @@ public class Barbare extends Personnage {
 	 */
 	private static final long serialVersionUID = 8067111339199337973L;
 
+	/**
+	 * Constructeur Barbare
+	 */
 	public Barbare() {
 		super("Barbare", 160, new Caracteristiques(200, 7, 14, 2, 5), 5);
 	}
 
-	@Override
+	/**
+	 * retourne l'image associée au barbare
+	 */
 	public String getPictureFileName() {
 		return "barbare.png";
 	}
