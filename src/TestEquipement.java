091d3caa0ffb20089aeadeb5131f114375259dc5
Florentin Le Moal <florentin.lemoal@gmail.com>
2014-01-07 14:34:14 +0100

    Permet de lancer un thread de création d’éléments

---
+++ src/TestEquipement.java
@@ -0,0 +1,15 @@
+import equipement.ApparitionEquipements;
+
+
+public class TestEquipement {
+
+	public TestEquipement() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ApparitionEquipements(10000).start();
+	}
+
+}
