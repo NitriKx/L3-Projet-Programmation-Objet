53eaf6dcb543f577bd88a4b95dce61c35c98a572
Benoît Sauvère <benoit.sauvere@teevity.com>
2014-01-07 09:33:04 +0100

    Changement du nom de l'application dans le dock MacOSX

--- src/TestIHM.java
+++ src/TestIHM.java
@@ -11,7 +11,9 @@ public class TestIHM {
 	public static void main(String[] args) {
 		// Placement des menus dans le menu-bar pour OS-X
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
-
+		// Changement du nom de l'application dans le dock OS-X
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Arène");
+		
 		IHM ihm=new IHM();
 		ihm.connect();
 		//boucle infine, interrompue par l'utilisateur (IHM)
